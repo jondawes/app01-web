e6ec2b7aa8d9d5bfba2e9c63bbd0bb5076ae407e
jondawes <58170791+jondawes@users.noreply.github.com>
2024-01-13 21:38:17 +1100

    Added About page

---
+++ pages/About.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+
+st.title("My Todo App")
+st.subheader("About the todo app")
+st.write("This app is part of the Udemy course. You can check it out at <a hrerf='https://www.udemy.com/course/the-python-mega-course/'>at Udemy</a>",
+        unsafe_allow_html=True)
